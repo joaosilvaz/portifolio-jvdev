812e4ce99f121b73661147e9138e62d10f611241
joaosilvaz <joaovitoraz31@gmail.com>
2025-05-07 15:37:01 -0300

    Alteração no page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,15 +2,12 @@ import { LocaleProvider } from '@/components/LocalProvider';
 import Main from '@/components/Main';
 
 type Props = {
-  params: { locale: string } | Promise<{ locale: string }>;
+  params: { locale: string };
 };
 
 export default async function Page({ params }: Props) {
-  // Aguarda a resolução de `params` se for uma Promise
-  const resolvedParams = await params;
-  const { locale } = resolvedParams;
+  const { locale } = params;
 
-  // Carrega as mensagens dinamicamente com base no `locale`
   const messages = (await import(`@/messages/${locale}.json`)).default;
 
   return (
@@ -19,4 +16,3 @@ export default async function Page({ params }: Props) {
     </LocaleProvider>
   );
 }
-
